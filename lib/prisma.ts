987ff52a44704cce89e801a0286e96dcb4eb9b75
theone14122000 <theone14122000@gmail.com>
2026-07-05 00:24:18 +0530

    almost all gradiant removed3

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,32 @@
-import { PrismaClient } from '@prisma/client';
+import "dotenv/config";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import { PrismaClient } from "../generated/prisma";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined };
+const globalForPrisma = globalThis as {
+  prisma?: PrismaClient;
+};
 
 function createPrismaClient() {
+  const adapter = new PrismaMariaDb({
+    host: "maglev.proxy.rlwy.net",
+    port: 38751,
+    user: "root",
+    password: "fgXrHJgxqnvddZNnfIaxlTBMABVkZqzx",
+    database: "railway",
+  });
+
   return new PrismaClient({
-    log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
+    adapter,
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["warn", "error"]
+        : ["error"],
   });
 }
 
-export const prisma = globalForPrisma.prisma ?? createPrismaClient();
+export const prisma =
+  globalForPrisma.prisma ?? createPrismaClient();
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
