ed47cfe2afd3ce56994946a94b2a653b088ec844
theone14122000 <theone14122000@gmail.com>
2026-07-05 00:34:18 +0530

    almost all gradiant removed5

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,8 +1,8 @@
-import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
+import "dotenv/config";
 
 export default defineConfig({
-  schema: "./prisma/schema.prisma",
+  schema: "prisma/schema.prisma",
   datasource: {
     url: env("DATABASE_URL"),
   },
